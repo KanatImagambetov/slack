7052de9329b0fcc257f80254a72fcc1e0d714043
KanatImagambetov <64198575+KanatImagambetov@users.noreply.github.com>
2020-05-08 14:37:25 -0500

    Update day_2.java

--- src/day_1/day_2.java
+++ src/day_1/day_2.java
@@ -3,7 +3,7 @@ package day_1;
 public class day_2 {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.print("fdsdsdsdss");
+        System.out.print("fdsddsdsdsaasaassdsdss");
         
     }
 }
